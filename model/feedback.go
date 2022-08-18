6795a14dc663ec9f72c19c8ada168840421d4eb6
mmelanti <prysacymelanti@gmail.com>
2022-08-18 12:22:06 +0700

    done model

---
+++ model/feedback.go
@@ -0,0 +1,16 @@
+package model
+
+import "gorm.io/gorm"
+
+type Feedback struct {
+	gorm.Model
+	ID        int     `json:"feedback_id" gorm:"primaryKey;not null"`
+	OrderId   int     `json:"order_id" gorm:"-;not null"`
+	Review    string  `json:"review" gorm:"size:100;not null"`
+	Rating    float32 `json:"rating" gorm:"-;not null"`
+	IsDeleted bool    `json:"is_deleted" gorm:"-;not null"`
+}
+
+func (Feedback) TableName() string {
+	return "mst_feedback"
+}
