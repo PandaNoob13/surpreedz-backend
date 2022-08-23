89c15d736273d353aa6e71c22ba1b62f1b6678d8
wil-irwan99 <91341314+wil-irwan99@users.noreply.github.com>
2022-08-23 15:18:53 +0700

    bugfix gorm.model

--- model/feedback.go
+++ model/feedback.go
@@ -3,12 +3,12 @@ package model
 import "gorm.io/gorm"
 
 type Feedback struct {
-	gorm.Model
 	ID        int     `json:"feedback_id" gorm:"primaryKey;not null"`
 	OrderId   int     `json:"order_id" gorm:";not null"`
 	Review    string  `json:"review" gorm:"size:100;not null"`
 	Rating    float32 `json:"rating" gorm:";not null"`
 	IsDeleted bool    `json:"is_deleted" gorm:";not null"`
+	gorm.Model
 }
 
 func (Feedback) TableName() string {
--- model/service_price.go
+++ model/service_price.go
@@ -7,11 +7,11 @@ import (
 )
 
 type ServicePrice struct {
-	gorm.Model
 	ID              int       `json:"service_price_id" gorm:"primaryKey;not null"`
 	ServiceDetailId int       `json:"service_detail_id" gorm:";not null"`
 	Price           int       `json:"price" gorm:";not null"`
 	Date            time.Time `json:"date" gorm:";not null"`
+	gorm.Model
 }
 
 func (ServicePrice) TableName() string {
